81970c9feb080f153920200c779a9c56b8c885ea
Casey Cross <hellocaseycross@gmail.com>
2016-12-07 15:18:08 -0700

    Have adapter shell built and create database.js

--- app/models/recipe.js
+++ app/models/recipe.js
@@ -1,5 +1,12 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+  name: DS.attr('string'),
+  photo: DS.attr('string'),
+  servings: DS.attr('number'),
+  time: DS.attr('string'),
+  ingredients: DS.hasMany('ingredient'),
+  directions: DS.hasMany('direction'),
+  notes: DS.attr('string'),
+  favorite: DS.attr('boolean', { defaultValue: false }),
 });
