43b2fc073495e9867b283f4d13881f861f2cfd0d
Casey Cross <hellocaseycross@gmail.com>
2016-12-10 23:32:22 -0700

    Convert entire project to jQuery and vanilla JS instead of Ember

---
+++ README.md
@@ -0,0 +1,31 @@
+# Recipes
+
+A desktop app to track and search recipes.
+
+## Prerequisites
+
+You will need the following things properly installed on your computer.
+
+* [Git](https://git-scm.com/)
+* [Node.js](https://nodejs.org/) (with NPM)
+
+## Installation
+* `git clone https://github.com/kccrs/recipes`
+* `cd recipes`
+* `npm install`
+
+
+### Running Tests
+
+<!-- * `ember test` -->
+
+### Building
+
+<!-- * `ember build` (development) -->
+<!-- * `ember build --environment production` (production) -->
+
+### Deploying
+<!-- TODO:  Specify what it takes to deploy your app.
+-->
+
+## Further Reading / Useful Links
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    main: './app/index.js',
+    test: 'mocha!./test/index-test.js'
+  },
+  output: {
+    path: __dirname,
+    filename: '[name].bundle.js'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      _: 'lodash'
+    })
+  ],
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: '/node_modules/', loader: 'babel-loader' },
+      { test: /\.css$/, loader: 'style!css' },
+      { test: /\.scss$/, loader: 'style!css!sass' }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.scss', '.css']
+  }
+};
